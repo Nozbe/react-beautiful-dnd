80c92519de43ad29a7126c5d133a28f19a29f6dc
radex <radexpl@gmail.com>
2023-02-22 14:14:34 +0100

    fix webpack warning

--- src/view/drag-drop-context/use-startup-validation.js
+++ src/view/drag-drop-context/use-startup-validation.js
@@ -1,9 +1,10 @@
 // @flow
 import React from 'react';
-import { peerDependencies } from '../../../package.json';
 import checkReactVersion from './check-react-version';
 import checkDoctype from './check-doctype';
 import useDevSetupWarning from '../use-dev-setup-warning';
+
+const { peerDependencies } = require('../../../package.json');
 
 export default function useStartupValidation(win: WindowProxy) {
   useDevSetupWarning(() => {
